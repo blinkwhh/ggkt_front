72c9d1701143d8e98b2dae2db073c5b522d880c9
e <1730121019@qq.com>
2022-11-15 16:35:18 +0800

    add method 'teacher add'

--- src/views/vod/teacher/form.vue
+++ src/views/vod/teacher/form.vue
@@ -49,18 +49,22 @@
     </el-form>
   </div>
 </template>
+
 <script>
 import teacherApi from '@/api/vod/teacher'
 export default {
   data() {
     return {
+      BASE_API: 'http://localhost:8301',
+      // 初始化讲师默认数据
       teacher:{
         sort: 0,
         level: 1
       },
-      BASE_API: 'http://localhost:8301',
+      saveBtnDisabled: false // 保存按钮是否禁用，防止表单重复提交
     }
   },
+  // 页面渲染成功
   created() {
     //获取路径id值，根据id查询得到数据
     if(this.$route.params.id) {
@@ -138,6 +142,8 @@ export default {
     },
     //添加和修改
     saveOrUpdate() {
+      // 禁用保存按钮
+      this.saveBtnDisabled = true
       if(!this.teacher.id) { //没有id，添加
         this.save()
       } else { //有id，修改
@@ -147,3 +153,28 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .avatar-uploader .avatar-uploader-icon {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar-uploader .avatar-uploader-icon:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader img {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
